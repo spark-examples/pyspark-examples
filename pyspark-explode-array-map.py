5531d0bbcaeb57af44cdc7223b7a8cb1155acc19
NNK <sparkbyexamples@gmail.com>
2020-02-01 19:51:20 -0800

    pyspark explode array

---
+++ pyspark-explode-array-map.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 22:42:50 2019
+
+@author: Naveen
+"""
+
+import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('pyspark-by-examples').getOrCreate()
+
+arrayData = [
+        ('James',['Java','Scala'],{'hair':'black','eye':'brown'}),
+        ('Michael',['Spark','Java',None],{'hair':'brown','eye':None}),
+        ('Robert',['CSharp',''],{'hair':'red','eye':''}),
+        ('Washington',None,None),
+        ('Jefferson',['1','2'],{})
+
+df = spark.createDataFrame(data=arrayData, schema = ['name','knownLanguages','properties'])
+df.printSchema()
+df.show()
+
+from pyspark.sql.functions import explode
+df2 = df.select(df.name,explode(df.knownLanguages))
+df2.printSchema()
+df2.show()
+
+from pyspark.sql.functions import explode
+df3 = df.select(df.name,explode(df.properties))
+df3.printSchema()
+df3.show()
+
+from pyspark.sql.functions import explode_outer
+""" with array """
+df.select(df.name,explode_outer(df.knownLanguages)).show()
+""" with map """
+df.select(df.name,explode_outer(df.properties)).show()
+
+
+from pyspark.sql.functions import posexplode
+""" with array """
+df.select(df.name,posexplode(df.knownLanguages)).show()
+""" with map """
+df.select(df.name,posexplode(df.properties)).show()
+
+from pyspark.sql.functions import posexplode_outer
+""" with array """
+df.select($"name",posexplode_outer($"knownLanguages")).show()
+
+""" with map """
+df.select(df.name,posexplode_outer(df.properties)).show()
+
+
+"""END"""
--- pyspark-explode-nested-array.py
+++ pyspark-explode-nested-array.py
@@ -2,22 +2,30 @@
 """
 Created on Thu Oct 24 22:42:50 2019
 
-@author: prabha
+@author: Naveen
 """
 
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, flatten
 
-spark = SparkSession.builder.appName('changeColNames').getOrCreate()
 
+spark = SparkSession.builder.appName('pyspark-by-examples').getOrCreate()
 
-dataDF = {'animal':['cat','dog','rat'],
-          'age':[10,60,50],
-          'visits':[1,2,3],
-          'priority':['yes','yes','no']
-          }
+arrayArrayData = [
+  ("James",[["Java","Scala","C++"],["Spark","Java"]]),
+  ("Michael",[["Spark","Java","C++"],["Spark","Java"]]),
+  ("Robert",[["CSharp","VB"],["Spark","Python"]])
+]
 
-df = spark.createDataFrame(data = [('Bob', 5.62,'juice'), 
-                                   ('Sue',0.85,'milk')], schema = ["Name", "Amount","Item"])
+df = spark.createDataFrame(data=arrayArrayData, schema = ['name','subjects'])
 df.printSchema()
 df.show(truncate=False)
+
+""" """
+df.select(df.name,explode(df.subjects)).show(truncate=False)
+
+""" creates a single array from an array of arrays. """
+df.select(df.name,flatten(df.subjects)).show(truncate=False)
+
+"""END"""
