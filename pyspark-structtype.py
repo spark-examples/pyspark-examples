ce1c8f84cfaa18219f3ff9b57433b7aabad8d8ae
NNK <sparkbyexamples@gmail.com>
2020-08-13 14:47:48 -0700

    pyspark-structtype

---
+++ pyspark-structtype.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+author SparkByExamples.com
+"""
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType,ArrayType,MapType
+from pyspark.sql.functions import col,struct,when
+
+spark = SparkSession.builder.master("local[1]") \
+                    .appName('SparkByExamples.com') \
+                    .getOrCreate()
+
+data = [("James","","Smith","36636","M",3000),
+    ("Michael","Rose","","40288","M",4000),
+    ("Robert","","Williams","42114","M",4000),
+    ("Maria","Anne","Jones","39192","F",4000),
+    ("Jen","Mary","Brown","","F",-1)
+  ]
+
+schema = StructType([ 
+    StructField("firstname",StringType(),True), 
+    StructField("middlename",StringType(),True), 
+    StructField("lastname",StringType(),True), 
+    StructField("id", StringType(), True), 
+    StructField("gender", StringType(), True), 
+    StructField("salary", IntegerType(), True) 
+  ])
+ 
+df = spark.createDataFrame(data=data,schema=schema)
+df.printSchema()
+df.show(truncate=False)
+
+structureData = [
+    (("James","","Smith"),"36636","M",3100),
+    (("Michael","Rose",""),"40288","M",4300),
+    (("Robert","","Williams"),"42114","M",1400),
+    (("Maria","Anne","Jones"),"39192","F",5500),
+    (("Jen","Mary","Brown"),"","F",-1)
+  ]
+structureSchema = StructType([
+        StructField('name', StructType([
+             StructField('firstname', StringType(), True),
+             StructField('middlename', StringType(), True),
+             StructField('lastname', StringType(), True)
+             ])),
+         StructField('id', StringType(), True),
+         StructField('gender', StringType(), True),
+         StructField('salary', IntegerType(), True)
+         ])
+
+df2 = spark.createDataFrame(data=structureData,schema=structureSchema)
+df2.printSchema()
+df2.show(truncate=False)
+
+
+updatedDF = df2.withColumn("OtherInfo", 
+    struct(col("id").alias("identifier"),
+    col("gender").alias("gender"),
+    col("salary").alias("salary"),
+    when(col("salary").cast(IntegerType()) < 2000,"Low")
+      .when(col("salary").cast(IntegerType()) < 4000,"Medium")
+      .otherwise("High").alias("Salary_Grade")
+  )).drop("id","gender","salary")
+
+updatedDF.printSchema()
+updatedDF.show(truncate=False)
+
+
+""" Array & Map"""
+
+
+arrayStructureSchema = StructType([
+    StructField('name', StructType([
+       StructField('firstname', StringType(), True),
+       StructField('middlename', StringType(), True),
+       StructField('lastname', StringType(), True)
+       ])),
+       StructField('hobbies', ArrayType(StringType()), True),
+       StructField('properties', MapType(StringType(),StringType()), True)
+    ])
+
