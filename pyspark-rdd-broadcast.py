cffd0c2fd33e70ba5598be8553e9d3509c059a14
NNK <sparkbyexamples@gmail.com>
2020-08-14 16:47:27 -0700

    PySpark examples

---
+++ pyspark-rdd-broadcast.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Sat Jan 11 19:38:27 2020
+
+@author: sparkbyexamples.com
+'''
+
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+states = {"NY":"New York", "CA":"California", "FL":"Florida"}
+broadcastStates = spark.sparkContext.broadcast(states)
+
+data = [("James","Smith","USA","CA"),
+    ("Michael","Rose","USA","NY"),
+    ("Robert","Williams","USA","CA"),
+    ("Maria","Jones","USA","FL")
+  ]
+
+rdd = spark.sparkContext.parallelize(data)
+
+def state_convert(code):
+    return broadcastStates.value[code]
+
+result = rdd.map(lambda x: (x[0],x[1],x[2],state_convert(x[3]))).collect()
+print(result)
+
---
+++ pyspark-select-columns.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 21:08:30 2020
+
+@author: NNK
+"""
+
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+data = [("James","Smith","USA","CA"),
+    ("Michael","Rose","USA","NY"),
+    ("Robert","Williams","USA","CA"),
+    ("Maria","Jones","USA","FL")
+  ]
+
+columns = ["firstname","lastname","country","state"]
+df = spark.createDataFrame(data = data, schema = columns)
+df.show(truncate=False)
+
+df.select("firstname").show()
+
+df.select("firstname","lastname").show()
+
+#Using Dataframe object name
+df.select(df.firstname,df.lastname).show()
+
+# Using col function
+from pyspark.sql.functions import col
+df.select(col("firstname"),col("lastname")).show()
+
+
+
+data = [
+        (("James",None,"Smith"),"OH","M"),
+        (("Anna","Rose",""),"NY","F"),
+        (("Julia","","Williams"),"OH","F"),
+        (("Maria","Anne","Jones"),"NY","M"),
+        (("Jen","Mary","Brown"),"NY","M"),
+        (("Mike","Mary","Williams"),"OH","M")
+        ]
+
+from pyspark.sql.types import StructType,StructField, StringType        
+schema = StructType([
+    StructField('name', StructType([
+         StructField('firstname', StringType(), True),
+         StructField('middlename', StringType(), True),
+         StructField('lastname', StringType(), True)
+         ])),
+     StructField('state', StringType(), True),
+     StructField('gender', StringType(), True)
+     ])
+
+
+df2 = spark.createDataFrame(data = data, schema = schema)
+df2.printSchema()
+df2.show(truncate=False) # shows all columns
+df2.select("name").show(truncate=False)
+df2.select("name.firstname","name.lastname").show(truncate=False)
+df2.select("name.*").show(truncate=False)
+
+
