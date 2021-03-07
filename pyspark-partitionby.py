6aeb9e8f9799de2eae181d4448356f4ae886eb4e
NNK <sparkbyexamples@gmail.com>
2021-03-06 22:47:23 -0800

    pyspark partitionby

---
+++ pyspark-partitionby.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+author SparkByExamples.com
+"""
+
+from pyspark.sql import SparkSession
+
+# Create SparkSession
+spark = SparkSession.builder \
+          .appName('SparkByExamples.com') \
+          .getOrCreate()
+
+#df=spark.read.option("header",True) \
+#        .csv("C:/apps/sparkbyexamples/src/pyspark-examples/resources/simple-zipcodes.csv")
+df=spark.read.option("header",True) \
+        .csv("C:/apps/sparkbyexamples/src/spark-scala-examples-new/src/main/resources/free-zipcode-database.csv")
+          
+df.show()
+print(df.rdd.getNumPartitions())
+
+df.write.option("header",True) \
+        .partitionBy("state") \
+        .mode("overwrite") \
+        .csv("c:/tmp/zipcodes-state")
+        
+df.write.option("header",True) \
+        .partitionBy("state","city") \
+        .mode("overwrite") \
+        .csv("c:/tmp/zipcodes-state-city")
+
+
+df=df.repartition(2)
+
+print(df.rdd.getNumPartitions())
+
+df.write.option("header",True) \
+        .partitionBy("state") \
+        .mode("overwrite") \
+        .csv("c:/tmp/zipcodes-state-more")
+        
+dfPartition=spark.read.option("header",True)\
+                 .csv("c:/tmp/zipcodes-state")
+dfPartition.printSchema()
+
+dfSinglePart=spark.read.option("header",True) \
+                  .csv("c:/tmp/zipcodes-state/state=AL/city=SPRINGVILLE")
+dfSinglePart.printSchema()
+dfSinglePart.show()
+
+parqDF = spark.read.option("header",True) \
+                  .csv("c:/tmp/zipcodes-state")
+parqDF.createOrReplaceTempView("ZIPCODE")
+spark.sql("select * from ZIPCODE  where state='AL' and city = 'SPRINGVILLE'") \
+    .show()
+
+df.write.option("header",True) \
+        .option("maxRecordsPerFile", 2) \
+        .partitionBy("state") \
+        .mode("overwrite") \
+        .csv("/tmp/zipcodes-state-maxrecords")
+
