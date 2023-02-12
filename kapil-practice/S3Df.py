3d3a2391a861ceb4db7b22dc56b5d5f5da241114
kapil <kapil.mnnit90@gmail.com>
2023-02-12 19:57:10 +0800

    aws s3 read file

---
+++ kapil-practice/S3Df.py
@@ -0,0 +1,25 @@
+
+
+from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.window import *
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.\
+master("local").\
+appName("prac").\
+getOrCreate()
+
+sc = spark.sparkContext
+sc._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
+sc._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+sc._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
+sc._jsc.hadoopConfiguration().set("fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A")
+sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.us-east-1.amazonaws.com")
+
+sc._jsc.hadoopConfiguration().set('fs.s3a.access.key', '')
+sc._jsc.hadoopConfiguration().set('fs.s3a.secret.key', '')
+sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.amazonaws.com")
+
+df = spark.read.option("inferSchema","true").option("header","true").csv("s3a://ind-north-up-agra-kapil-test-1/datasets/input-ds/movies/")
+df.show()
---
+++ kapil-practice/kp-convert-column-python-list.py
@@ -0,0 +1,15 @@
+from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master("local[1]") \
+    .appName('SparkByExamples.com') \
+    .getOrCreate()
+
+nasa_df = spark.read.json("G:\\datasets\\json-datasets\\nasa-singleline-zipcodes.json")
+nasa_df.printSchema()
+
+nasa_df.show()
+
+# all_cities = nasa_df.select("City").distinct().rdd.map(lambda x: x.City).collect()
+# print(all_cities)
+spark.stop()
