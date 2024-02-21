3b9e388d70ece7f443829aabf5573d8bb3778cd8
Md Shahid Afridi P <mdshahidafridip@gmail.com>
2024-02-21 13:50:40 +0530

    Update pyspark-add-month.py
    Straight forward solution to add months

--- pyspark-add-month.py
+++ pyspark-add-month.py
@@ -5,9 +5,6 @@ author SparkByExamples.com
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 
-from pyspark.sql.functions import col,expr
+from pyspark.sql.functions import add_months, to_date
 data=[("2019-01-23",1),("2019-06-24",2),("2019-09-20",3)]
-spark.createDataFrame(data).toDF("date","increment") \
-    .select(col("date"),col("increment"), \
-      expr("add_months(to_date(date,'yyyy-MM-dd'),cast(increment as int))").alias("inc_date")) \
-    .show()
+spark.createDataFrame(data,schema=["date","increment"]).select(['date','increment',add_months(to_date('date'),'increment').alias("inc_date")]).show()
