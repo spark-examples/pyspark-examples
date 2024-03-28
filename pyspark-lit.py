12973e5a2e8cf289ceeacf9df16467ad2dd99425
sagarzemosolabs <110030742+sagarzemosolabs@users.noreply.github.com>
2024-03-28 19:25:15 +0530

    Update pyspark-lit.py

--- pyspark-lit.py
+++ pyspark-lit.py
@@ -20,5 +20,5 @@ df2.show(truncate=False)
 
 
 from pyspark.sql.functions import when
-df3 = df2.withColumn("lit_value2", when(col("Salary") >=40000 & col("Salary") <= 50000,lit("100")).otherwise(lit("200")))
+df3 = df2.withColumn("lit_value2", when((col("Salary") >= 40000) & (col("Salary") <= 50000), lit("100")).otherwise(lit("200")))
 df3.show(truncate=False)
